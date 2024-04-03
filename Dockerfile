d635285b80d898629cb01977677b573326da0021
Andrey <harimambura@gmail.com>
2024-04-03 16:16:40 +0300

    test

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM golang:alpine
 WORKDIR /usr/local/bin
 RUN apk update && apk add wget
 RUN apk add nginx
+RUN apk add unzip
 RUN go install github.com/shadowsocks/go-shadowsocks2@latest
 
+COPY conf/ /conf
 COPY entrypoint.sh .
 RUN chmod +x /usr/local/bin/entrypoint.sh
 ENV PATH="$GOPATH/bin:/:$PATH"
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,11 @@ if [[ -z "${VER}" ]]; then
 fi
 echo ${VER}
 
+if [[ -z "${PORT}" ]]; then
+  PORT=443
+fi
+echo ${PORT}
+
 if [[ -z "${PASSWORD}" ]]; then
   PASSWORD="5c301bb8-6c77-41a0-a606-4ba11bbab084"
 fi
@@ -13,6 +18,11 @@ echo ${PASSWORD}
 if [[ -z "${ENCRYPT}" ]]; then
   ENCRYPT="chacha20-ietf-poly1305"
 fi
+
+if [[ -z "${X_Path}" ]]; then
+  X_Path="/s233"
+fi
+echo ${X_Path}
 
 
 
@@ -32,10 +42,21 @@ rm -rf xray-plugin-linux-amd64-$V_VER.tar.gz
 mv xray-plugin_linux_amd64 /xx-plugin
 rm -rf /xraybin
 
+sed -e "/^#/d"\
+    -e "s/\${PORT}/${PORT}/g"\
+    -e "s|\${X_Path}|${X_Path}|g"\
+    -e "$s"\
+    /conf/nginx_ss.conf > /etc/nginx/http.d/ss.conf
+echo /etc/nginx/http.d/ss.conf
+cat /etc/nginx/http.d/ss.conf
+
 
 plugin=$(echo -n "xray;path=${X_Path};host=${AppName}.onrender.com;tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
 ss="ss://$(echo -n ${ENCRYPT}:${PASSWORD} | base64 -w 0)@${AppName}.onrender.com:443?plugin=${plugin}" 
 echo ${ss}
 
 echo 'RUN SS SERVER'
-go-shadowsocks2 -s "ss://AEAD_CHACHA20_POLY1305:${PASSWORD}@:443" -plugin xx-plugin -plugin-opts "server;loglevel=none" -udp=false >> /dev/null
+go-shadowsocks2 -s "ss://AEAD_CHACHA20_POLY1305:${PASSWORD}@:2333" -plugin xx-plugin -plugin-opts "server;loglevel=none" -udp=false >> /dev/null
+
+rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
